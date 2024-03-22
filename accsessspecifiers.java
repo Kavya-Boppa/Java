f58d67c1d9e63834927c8762a717b22df157c401
Kavya-Boppa <157015371+Kavya-Boppa@users.noreply.github.com>
2024-03-22 15:48:13 +0530

    Add files via upload

---
+++ accsessspecifiers.java
@@ -0,0 +1,16 @@
+package accsessSpecifiers;
+
+public class accsessspecifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+   Demo d2 = new Demo();
+   System.out.println(d2.a);
+//   System.out.println(d2.a);
+//   System.out.println(d2.c);
+
+   
+	}
+
+}
